5459e96d501c4e6c28928f749b262214481eb121
gary-mayfield <gary.mayfield@hotmail.co.uk>
2018-05-05 08:49:27 +0100

    set up folders, framework and hello, world example.

---
+++ framework.c
@@ -0,0 +1,15 @@
+//
+// This is an example from Chapter 1" of
+// "The C Programming Language" by Brian Kernighan and Dennis Ritchie
+// - Gary Mayfield
+
+#include <stdio.h>
+
+//------------------------------------------------------------------------------
+
+int main()
+{
+
+}
+
+//------------------------------------------------------------------------------
