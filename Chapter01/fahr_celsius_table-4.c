6d147615f3d050b7336592dd1c9d8cec52fad269
gary-mayfield <gary.mayfield@hotmail.co.uk>
2018-05-05 10:21:21 +0100

    Finished exercise 1-3 and 1-4

--- Chapter01/fahr_celsius_table-4.c
+++ Chapter01/fahr_celsius_table-4.c
@@ -7,26 +7,26 @@
 
 //------------------------------------------------------------------------------
 
-/* print Fahrenheit-Celsius table
-    for fahr = 0, 20, ..., 300  - floating-point version */
+/* print Celsius-Fahrenheit table
+    for Celsius = -100, 20, ..., 100  - floating-point version */
 
 int main()
 {
     float fahr, celsius;
     int lower, upper, step;
 
-    lower = 0;                  // lower limit of temperature table
-    upper = 300;                // upper limit
+    lower = -100;                  // lower limit of temperature table
+    upper = 100;                // upper limit
     step = 20;                  // step size
 
-    fahr = lower;
-    printf("%s |  %s\n", "Fahrenheit", "Celsius");
+    celsius = lower;
+    printf("%s  |  %s\n", "Celsius", "Fahrenheit");
     printf("----------------------\n");
 
-    while (fahr <= upper){
-        celsius = 5 * (fahr-32) / 9;
-        printf("%3.0f\t   |  %6.4f\n", fahr, celsius);
-        fahr = fahr + step;
+    while (celsius <= upper){
+        fahr = celsius * 1.8 + 32;
+        printf("%4.0f     |  %6.2f\n", celsius, fahr);
+        celsius = celsius + step;
     }
 }
 
