2980957f59f7074ed45009eae29af05a705f8f48
Gary Mayfield <gary.mayfield@hotmail.co.uk>
2020-04-29 14:45:33 +0100

    simple python script to remove .obj and .exe files and keep dir clean

---
+++ delete_fragments.py
@@ -0,0 +1,13 @@
+import os, glob
+
+def main():
+
+    for filename in glob.iglob('./**', recursive=True):
+        if os.path.isfile(filename):
+            if filename.endswith('.obj') or filename.endswith('.exe'):
+                print(filename)
+                os.remove(filename)
+
+
+if __name__ == "__main__":
+    main() 
