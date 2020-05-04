a0786c7ef983c902ec707551077b3c6456e619d6
Gary Mayfield <gary.mayfield@hotmail.co.uk>
2020-05-04 09:00:58 +0100

    removal of uneeded comments

--- Chapter02/limits.c
+++ Chapter02/limits.c
@@ -1,5 +1,5 @@
 //
-// This is an exercise 2-1" of
+// This is an exercise" of
 // "The C Programming Language" by Brian Kernighan and Dennis Ritchie
 // - Gary Mayfield
 
@@ -9,31 +9,31 @@
 
 int main(void)
 {
-  printf("\nBits of type char: %d\n\n", CHAR_BIT);                     /* IV */
+  printf("\nBits of type char: %d\n\n", CHAR_BIT);                     
 
-  printf("Maximum numeric value of type char: %d\n", CHAR_MAX);        /* IV */
-  printf("Minimum numeric value of type char: %d\n\n", CHAR_MIN);      /* IV */
+  printf("Maximum numeric value of type char: %d\n", CHAR_MAX);        
+  printf("Minimum numeric value of type char: %d\n\n", CHAR_MIN);      
 
-  printf("Maximum value of type signed char: %d\n", SCHAR_MAX);        /* IV */
-  printf("Minimum value of type signed char: %d\n\n", SCHAR_MIN);      /* IV */
+  printf("Maximum value of type signed char: %d\n", SCHAR_MAX);        
+  printf("Minimum value of type signed char: %d\n\n", SCHAR_MIN);      
 
-  printf("Maximum value of type unsigned char: %u\n\n", (unsigned) UCHAR_MAX);    /* SF */  /* IV */
+  printf("Maximum value of type unsigned char: %u\n\n", (unsigned) UCHAR_MAX);
   
-  printf("Maximum value of type short: %d\n", SHRT_MAX);               /* IV */
-  printf("Minimum value of type short: %d\n\n", SHRT_MIN);             /* IV */
+  printf("Maximum value of type short: %d\n", SHRT_MAX);             
+  printf("Minimum value of type short: %d\n\n", SHRT_MIN);           
 
-  printf("Maximum value of type unsigned short: %u\n\n", (unsigned) USHRT_MAX);   /* SF */  /* IV */
+  printf("Maximum value of type unsigned short: %u\n\n", (unsigned) USHRT_MAX);   
 
 
-  printf("Maximum value of type int: %d\n", INT_MAX);                  /* IV */
-  printf("Minimum value of type int: %d\n\n", INT_MIN);                /* IV */
+  printf("Maximum value of type int: %d\n", INT_MAX);                  
+  printf("Minimum value of type int: %d\n\n", INT_MIN);                
 
-  printf("Maximum value of type unsigned int: %u\n\n", UINT_MAX);      /* RB */   /* IV */
+  printf("Maximum value of type unsigned int: %u\n\n", UINT_MAX);      
 
-  printf("Maximum value of type long: %ld\n", LONG_MAX);               /* RB */   /* IV */
-  printf("Minimum value of type long: %ld\n\n", LONG_MIN);             /* RB */   /* IV */
+  printf("Maximum value of type long: %ld\n", LONG_MAX);               
+  printf("Minimum value of type long: %ld\n\n", LONG_MIN);             
 
-  printf("Maximum value of type unsigned long: %lu\n\n", ULONG_MAX);   /* RB */   /* IV */
+  printf("Maximum value of type unsigned long: %lu\n\n", ULONG_MAX);
 
 
   return 0;
